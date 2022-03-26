34a6c48d1e257ea28d78042c68415519a1fbc07c
Christian Grinling <grinlingc@gmail.com>
2022-03-26 10:42:06 +0000

    lol

--- csv_test.py
+++ csv_test.py
@@ -1,11 +1,23 @@
+# Importing the required libraries
 import csv
-rows = []
+import pandas as pd
 
+tab_file = "example.tab"
+csv_file = "tab_to_csv.csv"
+
+dataframe = pd.read_csv(tab_file,delimiter="\t")
+dataframe.to_csv(csv_file, encoding='utf-8', index=False)
+
+"""
 def importCSV(csvfile):
     print("Importing CSV")
     with open(csvfile, 'r') as file:
         csvreader = csv.reader(file)
         header = next(csvreader)
-    for row in csvreader:
-        rows.append(row)
+        print(header)
+        for row in csvreader:
+            rows.append(row)
+            print(row)
 
+importCSV('exampledata.csv')
+"""
