a2e88f29c96646b8f782d3337f264882c883694b
Artemiy Malyshau <80065244+Nkoorty@users.noreply.github.com>
2022-03-26 11:01:09 +0000

    Update csv_test.py

--- csv_test.py
+++ csv_test.py
@@ -3,15 +3,10 @@ import csv
 import pandas as pd
 
 rows = []
-#tab_file = "example.tab"
-#csv_file = "tab_to_csv.csv"
 
-#dataframe = pd.read_csv(tab_file,delimiter="\t")
-#dataframe.to_csv(csv_file, encoding='utf-8', index=False)
-
-def importCSV(csvfile):
+def importCSV(filename):
     print("Importing CSV")
-    with open(csvfile, 'r') as file:
+    with open(filename, 'r') as file:
         csvreader = csv.reader(file)
         header = next(csvreader)
         print(header)
