2aced3b6acbcfa73c4704c29c9f5e7b5fce6ba12
Christian Grinling <grinlingc@gmail.com>
2022-03-26 12:01:23 +0000

    Update csv_test.py

--- csv_test.py
+++ csv_test.py
@@ -1,11 +1,19 @@
 # Importing the required libraries
 import csv
 import pandas as pd
-import keras
-import numpy
+import numpy as np
 
+# Make numpy values easier to read.
+np.set_printoptions(precision=3, suppress=True)
+
+import tensorflow as tf
+from tensorflow.keras import layers
+
+#Import OS for reading in files
+import os
+
+#Import CSV File Function Handler
 rows = []
-
 def importCSV(filename):
     print("Importing CSV")
     with open(filename, 'r') as file:
@@ -16,7 +24,17 @@ def importCSV(filename):
             rows.append(row)
             print(row)
 
-importCSV('test.csv')
-wavelength = rows[0]
-reflectance = rows[1]
-uncertainty = rows[2]
+
+
+#Import CSV File
+#importCSV('test.csv')
+
+#Define Wavelength, Reflectance and Uncertainty Variables
+#wavelength = rows[0]
+#reflectance = rows[1]
+#uncertainty = rows[2]
+
+#Import data into a dataframe
+#asteroid_train = pd.read_csv('test.csv')
+#asteroid_train.head()
+#(train_data, train_labels), (test_data, test_labels) = 
